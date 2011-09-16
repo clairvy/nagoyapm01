ab32fa5aa407fc79adeab16922bbff66ac00db1b
NAGAYA Shinichiro <clairvy@gmail.com>
2011-09-16 15:44:56 +0900

    append makefile for perl5

---
+++ perl5/lib/Check.pm
@@ -0,0 +1,19 @@
+package Check;
+
+=head1 AUTHOR
+
+NAGAYA Shinichiro<clairvy@gmail.com>
+
+=head1 LICENSE
+
+Copyright (c) NAGAYA Shinichiro
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+
+=cut
+
+use 5.005;
+
+our $VERSION = '1.0';
+
+1;
